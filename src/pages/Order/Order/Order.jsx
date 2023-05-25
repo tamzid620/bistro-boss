c7e43104cbcd66f9b78fcc24d216eaf45bbd439f
tamzid620 <tamzid620@gmail.com>
2023-05-25 21:36:45 +0600

    react tab installed and order added

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -1,9 +1,38 @@
-
+import { useState } from "react";
+import orderCoverImg from "../../../assets/shop/order.jpg"
+import Cover from "../../shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import useMenu from "../../../Hooks/useMenu";
 
 const Order = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert')
+    const soup = menu.filter(item => item.category === 'soup');
+    const salad = menu.filter(item => item.category === 'salad');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const offered = menu.filter(item => item.category === 'offered');
+    
     return (
         <div>
-            
+            <Cover img={orderCoverImg} title="Order Food"></Cover>
+            <div className="my-5">
+                <Tabs defaultIndex={1} onSelect={(index) => setTabIndex(index)}>
+                    <TabList>
+                        <Tab>SALAD</Tab>
+                        <Tab>PIZZA</Tab>
+                        <Tab>SOUP</Tab>
+                        <Tab>DESSERT</Tab>
+                        <Tab>DRINKS</Tab>
+                    </TabList>
+                    <TabPanel></TabPanel>
+                    <TabPanel></TabPanel>
+                    <TabPanel></TabPanel>
+                    <TabPanel></TabPanel>
+                    <TabPanel></TabPanel>
+                </Tabs>
+            </div>
         </div>
     );
 };
