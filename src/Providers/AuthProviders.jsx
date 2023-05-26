9f9a7f492b2505962a2ced658c72a7207e46dcc3
tamzid620 <tamzid620@gmail.com>
2023-05-26 15:20:52 +0600

    env.locaal file info added

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile } from "firebase/auth";
-import { app } from "../firebase/firebase.config";
+import app from "../Firebase/firebase.config";
+
 
 export const AuthContext = createContext(null);
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,18 @@
-import { useContext, useEffect, useState } from 'react';
+// import {  useContext, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
-import { AuthContext } from '../../providers/AuthProvider';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
-import { Helmet } from 'react-helmet-async';
-import Swal from 'sweetalert2'
+// import { AuthContext } from '../../Providers/AuthProviders';
+// import { Link, useLocation, useNavigate } from 'react-router-dom';
+// import Swal from 'sweetalert2'
 
 const Login = () => {
     const [disabled, setDisabled] = useState(true);
-    const { signIn } = useContext(AuthContext);
-    const navigate = useNavigate();
-    const location = useLocation();
+    // const { signIn } = useContext(AuthContext);
+    // const navigate = useNavigate();
+    // const location = useLocation();
 
-    const from = location.state?.from?.pathname || "/";
+    // const from = location.state?.from?.pathname || "/";
 
     useEffect(() => {
         loadCaptchaEnginge(6);
@@ -23,21 +24,21 @@ const Login = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
-        signIn(email, password)
-            .then(result => {
-                const user = result.user;
-                console.log(user);
-                Swal.fire({
-                    title: 'User Login Successful.',
-                    showClass: {
-                        popup: 'animate__animated animate__fadeInDown'
-                    },
-                    hideClass: {
-                        popup: 'animate__animated animate__fadeOutUp'
-                    }
-                });
-                navigate(from, { replace: true });
-            })
+        // signIn(email, password)
+        //     .then(result => {
+        //         const user = result.user;
+        //         console.log(user);
+        //         Swal.fire({
+        //             title: 'User Login Successful.',
+        //             showClass: {
+        //                 popup: 'animate__animated animate__fadeInDown'
+        //             },
+        //             hideClass: {
+        //                 popup: 'animate__animated animate__fadeOutUp'
+        //             }
+        //         });
+        //         navigate(from, { replace: true });
+        //     })
     }
 
     const handleValidateCaptcha = (e) => {
@@ -52,9 +53,6 @@ const Login = () => {
 
     return (
         <>
-            <Helmet>
-                <title>Bistro Boss | Login</title>
-            </Helmet>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col md:flex-row-reverse">
                     <div className="text-center md:w-1/2 lg:text-left">
